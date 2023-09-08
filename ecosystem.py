b29834ef33b2153c415f133ea324bc7b21fc05d8
Justin <J.L.Oosterbaan@student.tudelft.nl>
2023-09-08 16:22:10 +0200

    Move OSCMessage function to ecosystem.py

--- ecosystem.py
+++ ecosystem.py
@@ -1,5 +1,7 @@
 from components.wifi_setup import WiFi
 from components.mqtt_setup import MQTTBroker
+from SimpleOSC import SimpleOSCParser
+from IEOSC import IEOSC
 from timer import Timer
 import random
 
@@ -13,22 +15,36 @@ class EcoSystem:
     def __init__(self, ecosystem, creature, connect_to_ecosystem):
         self.connect_to_ecosystem = connect_to_ecosystem
         self.creature = creature
-
         if self.connect_to_ecosystem:
-            wifi = WiFi()
-            self.mqtt = MQTTBroker(wifi, ecosystem, self.creature)
+            self.wifi = WiFi()
+            self.parser = SimpleOSCParser()
+            self.OSCCommunicator = IEOSC(self.wifi.esp, self.wifi.socket, self.parser)
 
     # Sends a message in the ecosystem
     def send_message(self, message):
         print("sending: " + message)
-        if self.connect_to_ecosystem:
-            self.mqtt.send(self.mqtt.client_id + "$$$" + message)
+#         if self.connect_to_ecosystem:
+#             self.mqtt.send(self.mqtt.client_id + "$$$" + message)
+
+    def OSCMessage(self, topic, dataTypes, output):
+        global lightOn
+        global noise
+#         print(topic)
+#         print(dataTypes)
+#         print(output)
+        if topic == "/reefcontrol/timeofday":
+            # Update the time of day
+            self.creature.time_of_day = int(output[0])
+            print("Updated time of day " + str(self.creature.time_of_day))
+        if topic == "/reefcontrol/energy":
+            # Update the time of day
+            self.creature.energy = int(output[0])
 
     # Checks if there is a message from the eco system
     def check_for_messages(self):
         global offline_messages, offline_timer
         if self.connect_to_ecosystem:
-            self.mqtt.loop()
+            self.OSCCommunicator.loop(self.OSCMessage)
         else:
             # fake the ecosystem
             if offline_timer.expired():
---
+++ lib/IEOSC.py
@@ -0,0 +1,44 @@
+import time
+from SimpleOSC import SimpleOSCParser
+
+class IEOSC:
+    def __init__(self, esp, socket, parser, _HOST="192.168.231.175", _PORT=1212):
+        self.HOST = _HOST
+        self.PORT = _PORT
+        self.TIMEOUT = 5
+        self.INTERVAL = 5
+        self.MAXBUF = 256
+        self.parser = parser
+        self.esp = esp
+        self.socket = socket
+        print("Create UDP Client Socket")
+        self.s = socket.socket(type=socket.SOCK_DGRAM)
+        self.s.settimeout(self.TIMEOUT)
+
+        print("Connecting")
+        self.socketaddr = socket.getaddrinfo(self.HOST, self.PORT)[0][4]
+        self.socketaddrSend = socket.getaddrinfo(self.HOST, 1213)[0][4]
+        self.s.connect(self.socketaddr, conntype=esp.UDP_MODE)
+#         print("Server ping", esp.ping(self.HOST), "ms")
+
+
+    def loop(self, cb):
+        if self.s._available() > 0:
+            buf = self.s.recv(self.MAXBUF)
+#             print(buf)
+            topic, types, output = self.parser.parseOSC(buf)
+            if topic:
+                cb(topic, types, output)
+
+    def sendMessage(self, message):
+        self.s.close()
+        self.s.connect(self.socketaddrSend, conntype=self.esp.UDP_MODE)
+        print("sending: ", message)
+        try:
+            self.s.send(message)
+        except:
+            print("Failed to send message, trying again")
+            self.sendMessage(message)
+        finally:
+            self.s.close()
+            self.s.connect(self.socketaddr, conntype=self.esp.UDP_MODE)
